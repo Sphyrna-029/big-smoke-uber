68d578cc6f85dfc593bc4b0c5bd9befd2df691ce
Joshua <9244335+Sphyrna-029@users.noreply.github.com>
2025-01-19 00:29:32 -0500

    Exclusion Zone

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # big-smoke-uber
 Journey to a self driving car in GTA
 
+Requires CUDA Tool Kit 12.4
+
+'''
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+pip3 install requirements.txt
+'''
+
 ![demo](https://github.com/Sphyrna-029/big-smoke-uber/blob/main/Demo.PNG?raw=true)
--- capturescreen.py
+++ capturescreen.py
@@ -2,15 +2,25 @@ import cv2
 import numpy
 import time
 import mss
-import keyboard
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+from pynput import keyboard
+from PIL import Image
 from ultralytics import YOLO
+
+
+###################### CONFIG ######################
 
 # Declare yolov11 model
 model = YOLO("yolo11n.pt")
+exemption_zone = Polygon([(0, 800), (340, 340), (470, 340), (800, 800)])
+
+###################### CONFIG ######################
+
 
 def predict(chosen_model, img, classes=[], conf=0.5):
     if classes:
-        results = chosen_model.predict(img, classes=classes, conf=conf)
+        results = chosen_model.predict(img, classes=classes, conf=conf, device=0)
 
     else:
         results = chosen_model.predict(img, conf=conf)
@@ -22,20 +32,56 @@ def predict_and_detect(chosen_model, img, classes=[], conf=0.5, rectangle_thickn
     results = predict(chosen_model, img, classes, conf=conf)
     for result in results:
         for box in result.boxes:
+
+            #Check if detected object is in exclusion zone. 
+            middle = ((int(box.xyxy[0][0]) + int(box.xyxy[0][2])) / 2, (int(box.xyxy[0][1]) + int(box.xyxy[0][3])) / 2)
+            middle = Point(middle)
+
+            if exemption_zone.contains(middle):
+                color = (255, 0, 0)
+            
+            else:
+                color = (0, 128, 0)
+
             cv2.rectangle(img, 
                          (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
                          (int(box.xyxy[0][2]), int(box.xyxy[0][3])), 
-                         (255, 0, 0), rectangle_thickness)
+                         color, rectangle_thickness)
 
             cv2.putText(img, 
                         f"{result.names[int(box.cls[0])]}",
                         (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
-                        cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), text_thickness)
+                        cv2.FONT_HERSHEY_PLAIN, 1, color, text_thickness)
 
     return img, results
 
 
+def on_press(key):
+    global active_key
 
+    try:
+        active_key = key.char
+
+    except AttributeError:
+        active_key = key
+
+
+def on_release(key):
+    global active_key
+
+    active_key = "No Action"
+
+'''
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+'''
+
+
+listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+listener.start()
+
+time.sleep(1)
 with mss.mss() as sct:
     # Part of the screen to capture
     monitor = {"top": 240, "left": 560, "width": 800, "height": 600}
@@ -46,24 +92,27 @@ with mss.mss() as sct:
 
         # Get raw pixels from the screen, save it to a Numpy array
         img = numpy.array(sct.grab(monitor))
+
+        # Convert to RGB color space
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        img, _ = predict_and_detect(model, img, classes=[9, 2, 7, 11, 0], conf=0.5)
+        # Detect objects in our frame
+        img, _ = predict_and_detect(model, img, classes=[9, 2, 7, 11, 0], conf=0.7)
 
         # Get FPS
         fps = "FPS: " + str(round(1 / (time.time() - last_time)))
 
-        #Draw FPS over image
+        # Draw FPS over image
         img = cv2.putText(img, fps, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        img = cv2.putText(img, str(active_key), (50, 75), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # Draw exclusion zone
+        e_zone = numpy.array([[0, 800],[340, 340],[470, 340],[800, 800]], numpy.int32)
+        e_zone = e_zone.reshape((-1,1,2))
+        img = cv2.polylines(img, [e_zone], True, (255, 0, 0), 2)
 
         # Display the picture
         cv2.imshow("Big Smoke Uber", img)
-
-        # Display the picture in grayscale
-        # cv2.imshow('OpenCV/Numpy grayscale',
-        #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
-
-        #print(f"fps: {1 / (time.time() - last_time)}")
 
         # Press "q" to quit
         if cv2.waitKey(25) & 0xFF == ord("q"):
